e58452af81ca11281bfb7b02c941120c0e25e488
Arda Kazancı <ardakazanci@anadolu.edu.tr>
2018-04-02 23:31:11 +0300

    Sample Project
    Sample Project

--- AndroidTutorialDayChallenge/D1_ActivityOrientation/app/src/main/java/com/ardakazanci/d1_activityorientation/MainActivity.java
+++ AndroidTutorialDayChallenge/D1_ActivityOrientation/app/src/main/java/com/ardakazanci/d1_activityorientation/MainActivity.java
@@ -1,12 +1,18 @@
 package com.ardakazanci.d1_activityorientation;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     // B PLANI : manifest dosyasında : android:screenOrientation="landscape" vermek yeterlidir.
 
+    // Activity ' nin istenilen yönde başlatılmasını sağlamak
+
+    Button button_1,button_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        getControlView();
+        clickForView();
+
 
     }
+
+    private void getControlView(){
+
+        button_1 = findViewById(R.id.button_1); //
+        button_2 = findViewById(R.id.button_2);
+
+    }
+
+
+    private void clickForView(){
+
+
+        button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // Yatay
+
+
+            }
+        });
+
+        button_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // Dikey
+
+            }
+        });
+
+
+    }
+
 }
